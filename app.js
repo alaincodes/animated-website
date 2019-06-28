c2b41ddd6ba9b8bb2ca12e9d36a9743fd2177ca9
Alain <alaint77@gmail.com>
2019-06-28 19:16:31 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector(".nav-container");
+const boxing = document.querySelector("#box");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("toggle");
+});
