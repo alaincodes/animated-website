062361fa74bf08573ea44cb9405e6a7094ca1c48
Alain <alaint77@gmail.com>
2019-06-29 12:36:45 +0200

    add working burger

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,29 @@
 const burger = document.querySelector(".nav-container");
 const switcher = document.getElementById("switchLang");
 const languageText = document.getElementById("languageChange");
-const h1 = document.getElementById("languageText");
+const textChange = document.getElementById("languageText");
+const menu = document.getElementById("nav-links");
+const switcherBtn = document.querySelector(".switch");
 
+// BURGER MENU CONFIG
 burger.addEventListener("click", () => {
-  burger.classList.toggle("toggle");
+  burger.classList.toggle("toggle")
+    ? menu.setAttribute("style", "opacity: 1") &&
+      switcherBtn.setAttribute("style", "opacity: 0")
+    : menu.setAttribute("style", "opcity: 0") &&
+      switcherBtn.setAttribute("style", "opacity: 1");
 });
 
+// SWITCH LANG CONFIG
 switcher.addEventListener("click", () => {
   if (
-    h1.innerHTML ===
+    textChange.innerHTML ===
     "Je suis John, <br> Développeur JavaScript Jr. <br> Je construis des websites."
   ) {
-    h1.innerHTML =
+    textChange.innerHTML =
       "I'm John, <br>Jr. JavaScript Developer, <br>I code for web and mobile devices.";
   } else {
-    h1.innerHTML =
+    textChange.innerHTML =
       "Je suis John, <br> Développeur JavaScript Jr. <br> Je construis des websites.";
   }
 });
