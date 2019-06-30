25522417d2e9ec04fc844994ae38956ea75f04bd
Alain <alaint77@gmail.com>
2019-06-30 23:03:45 +0200

    add rainbow animation. Need to change to displayNone instead of opacity0.....

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,17 @@
 const burger = document.querySelector(".burger");
 const switcher = document.getElementById("switchLang");
 const textChange = document.getElementById("languageText");
-const menu = document.getElementById("nav-links");
-const switcherBtn = document.querySelector(".switch");
+const menu = document.querySelector(".nav-links");
 
 // BURGER MENU CONFIG
 
 burger.addEventListener("click", () => {
   if (burger.classList.toggle("toggle")) {
     menu.style.opacity = 1;
-    switcherBtn.style.opacity = 0;
+    switcher.style.opacity = 0;
   } else {
     menu.style.opacity = 0;
-    switcherBtn.style.opacity = 1;
+    switcher.style.opacity = 1;
   }
 });
 
@@ -20,12 +19,12 @@ burger.addEventListener("click", () => {
 switcher.addEventListener("click", () => {
   if (
     textChange.innerHTML ===
-    "Je suis John, <br> Développeur JavaScript Jr. <br> Je construis des websites."
+    "Je suis John, <br> Développeur JavaScript, <br> Je construis des websites."
   ) {
     textChange.innerHTML =
       "I'm John, <br>Jr. JavaScript Developer, <br>I code for web and mobile devices.";
   } else {
     textChange.innerHTML =
-      "Je suis John, <br> Développeur JavaScript Jr. <br> Je construis des websites.";
+      "Je suis John, <br> Développeur JavaScript, <br> Je construis des websites.";
   }
 });
